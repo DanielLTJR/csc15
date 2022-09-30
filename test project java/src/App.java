150a7e6ecbd48872c0a3d3c681a2ff4b6e062119
seatbuckle <joseefable@gmail.com>
2022-09-30 11:47:14 -0700

    calc foundation still working

--- test project java/src/App.java
+++ test project java/src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //Making Scanner
+        Scanner calculator = new Scanner(System.in);
+        // Initializing Variables
+        int v1 = 0;
+        int v2 = 0;
+        int choice = 0;
+        boolean done = false;
+        // Asking for v1 , handles incorrect values
+        while(!done){
+            try{
+                System.out.println("Please enter a value");
+                v1 = calculator.nextInt();
+                done = true;
+            }catch(InputMismatchException e){
+                calculator.nextLine();
+                System.out.println("Invalid. Enter a whole integer");
+            }
+        }
+        
     }
 }
